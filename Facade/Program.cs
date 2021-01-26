24a634cc3412bab68f845fccddc59a1e45988bd0
HeberAtSnow <heber.allen@snow.edu>
2021-01-26 15:23:53 -0700

    Serilog:  logfile, database both working.  Some via appsettings.json, some via CreateHostBuilder in Program.cs

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -45,8 +45,8 @@ namespace Facade
                 .UseSerilog((hostingContext, services, loggerConfiguration) => loggerConfiguration
                     .ReadFrom.Configuration(hostingContext.Configuration)
                     //.Enrich.FromLogContext()
-                    //.WriteTo.File("/logs/Facade.log")
-                    //.WriteTo.PostgreSQL(hostingContext.Configuration["db_connectionstring"], "st_facade", batchSizeLimit: 1, needAutoCreateTable: true)
+                    .WriteTo.File("/logs/Facade.log")
+                    .WriteTo.PostgreSQL(hostingContext.Configuration["db_connectionstring"], "st_facade", batchSizeLimit: 1, needAutoCreateTable: true)
                     
                     )
                 .ConfigureWebHostDefaults(webBuilder =>
